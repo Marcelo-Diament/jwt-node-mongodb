341ff2ee0adbb8405f48861d7bc69a88941bbf93
Marcelo-Diament <marcelodiament@gmail.com>
2021-01-17 21:58:25 -0300

    feature: server and initial route

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => res.send('<h1>Node + Express + MongoDB Auth</h1>'))
+
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
