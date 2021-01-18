5b4f043a2f1b78ff85529f427a87bc3cd0073e52
Marcelo-Diament <marcelodiament@gmail.com>
2021-01-17 22:36:27 -0300

    feature: auth middleware import

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const authMiddleware = require('../middlewares/auth')
+
 const router = express.Router()
+
+router.use(authMiddleware)
 
 router.get('/', async (req, res) => res.send({ ok: true, userId: req.userId }))
 
