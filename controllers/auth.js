77b6d0d4fcb89ad0965f5d7d6f8485b3f388abc9
Marcelo-Diament <marcelodiament@gmail.com>
2021-01-17 22:20:14 -0300

    feature: registration validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,13 @@ router.post('/register', async (req, res) => {
   const { email } = req.body
 
   try {
+    if ((await User.findOne({ email: email })))
+      return res.status(400).send({ error: 'Ops! Parece que esse email já foi cadastrado!' })
+
     const user = await User.create(req.body)
+
+    user.senha = undefined
+
     return res.send({ user })
   } catch (err) {
     return res.status(400).send({ error: 'Ops! Houve uma falha no cadastro do usuário' })
